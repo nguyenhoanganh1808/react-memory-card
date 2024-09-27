cc0a755cfd6003288c274240628628cf75442046
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2024-09-27 16:56:33 +0700

    change font

---
+++ src/pages/GameBoard.jsx
@@ -0,0 +1,11 @@
+export default function GameBoard() {
+  return (
+    <header>
+      <h1>Memory Card Game</h1>
+      <p>
+        Get points by clicking on an image but don`&apos`t click on any more
+        than once!
+      </p>
+    </header>
+  );
+}
